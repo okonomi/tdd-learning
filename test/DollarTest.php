2e017291f82d688916e9b9560fd5da2070c2f132
okonomi <oknm@okonomi.sakura.ne.jp>
2011-07-05 21:07:41 +0900

    操作を行ってもDollarクラスのオブジェクトが変化しないようにしたい。

--- test/DollarTest.php
+++ test/DollarTest.php
@@ -8,7 +8,9 @@ class DollarTest extends PHPUnit_Framework_TestCase
     public function testMultiplication()
     {
         $five = new Dollar(5);
-        $five->times(2);
-        $this->assertEquals(10, $five->amount);
+        $product = $five->times(2);
+        $this->assertEquals(10, $product->amount);
+        $product = $five->times(3);
+        $this->assertEquals(15, $product->amount);
     }
 }
