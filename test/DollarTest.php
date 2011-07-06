bdfbf880d4f8e9af3a328d60e51283785d3f608b
okonomi <oknm@okonomi.sakura.ne.jp>
2011-07-06 22:04:02 +0900

    フラン通貨のテストを追加

--- test/DollarTest.php
+++ test/DollarTest.php
@@ -12,6 +12,13 @@ class DollarTest extends PHPUnit_Framework_TestCase
         $this->assertEquals(new Dollar(15), $five->times(3));
     }
 
+    public function testFrancMultiplication()
+    {
+        $five = new Franc(5);
+        $this->assertEquals(new Franc(10), $five->times(2));
+        $this->assertEquals(new Franc(15), $five->times(3));
+    }
+
     public function testEquality()
     {
         $five = new Dollar(5);
