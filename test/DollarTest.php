7044f02a9fb78875e28d4b6f26c0cb9ad55f9950
okonomi <oknm@okonomi.sakura.ne.jp>
2011-07-10 19:51:43 +0900

    派生クラスを削除して、テストを整理。

--- test/DollarTest.php
+++ test/DollarTest.php
@@ -1,7 +1,6 @@
 <?php
 
-require_once 'Dollar.php';
-require_once 'Franc.php';
+require_once 'Money.php';
 
 
 class DollarTest extends PHPUnit_Framework_TestCase
@@ -13,20 +12,10 @@ class DollarTest extends PHPUnit_Framework_TestCase
         $this->assertTrue(Money::dollar(15)->equals($five->times(3)));
     }
 
-    public function testFrancMultiplication()
-    {
-        $five = Money::franc(5);
-        $this->assertTrue(Money::franc(15)->equals($five->times(3)));
-    }
-
     public function testEquality()
     {
         $this->assertTrue(Money::dollar(5)->equals(Money::dollar(5)));
         $this->assertFalse(Money::dollar(5)->equals(Money::dollar(6)));
-
-        $this->assertTrue(Money::franc(5)->equals(Money::franc(5)));
-        $this->assertFalse(Money::franc(5)->equals(Money::franc(6)));
-
         $this->assertFalse(Money::franc(5)->equals(Money::dollar(5)));
     }
 
@@ -34,11 +23,5 @@ class DollarTest extends PHPUnit_Framework_TestCase
     {
         $this->assertEquals('USD', Money::dollar(1)->currency());
         $this->assertEquals('CHF', Money::franc(1)->currency());
-    }
-
-    public function testDifferentClassEquality()
-    {
-        $money = new Money(10, 'CHF');
-        $this->assertTrue($money->equals(new Franc(10, 'CHF')));
     }
 }
